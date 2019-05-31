059a7e62bc86999efd8ecf49da2f86d7f88ed842
cyantis <cyantis@gmail.com>
2019-05-30 19:29:07 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find(params[:username], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    redirect '/error'
   end
 
   get '/account' do
-
+    @user = User.find(session[:user_id])
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
 end
-
